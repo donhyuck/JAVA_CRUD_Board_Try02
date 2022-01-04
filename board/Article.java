143fc395975eff937207e3c869c168bcab6c2dc1
donhyuck <leedonhyuck123@gmail.com>
2022-01-04 22:15:30 +0900

    List 이용, 게시글 작성 기능 구현

---
+++ board/Article.java
@@ -0,0 +1,9 @@
+package board;
+
+public class Article {
+
+	public Article(int id, String title, String body) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- board/BoardMain.java
+++ board/BoardMain.java
@@ -1,11 +1,19 @@
 package board;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BoardMain {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
+
+		// Article 로 게시글 정보를 연결한다.
+		List<Article> articles = new ArrayList<>();
+
+		// 게시글 작성마다 번호를 부여한다.
+		int lastId = 1;
 
 		while (true) {
 			System.out.print("명령어 : ");
@@ -27,7 +35,7 @@ public class BoardMain {
 			} else if (command.equals("article write")) {
 				System.out.println("== 게시글 작성 ==");
 
-				int id = 1;
+				int id = lastId++;
 
 				System.out.print("글 제목 : ");
 				String title = sc.nextLine();
@@ -35,7 +43,11 @@ public class BoardMain {
 				System.out.print("글 내용 : ");
 				String body = sc.nextLine();
 
-				System.out.printf("%d번 글이 작성되었습니다.\n", id);
+				Article article = new Article(id, title, body);
+				
+				articles.add(article);
+
+				System.out.printf("%d번 글이 등록되었습니다.\n", id);
 
 			} else {
 				System.out.printf("%s는 존재하지 않는 명령어입니다.\n", command);
